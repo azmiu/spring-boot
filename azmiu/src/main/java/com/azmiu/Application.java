6544077a34ee401e9c2b46b18cc45da14c47d813
azmiu <zz120619@163.com>
2017-08-21 17:05:11 +0800

    delete rubbish codes

--- azmiu/src/main/java/com/azmiu/Application.java
+++ azmiu/src/main/java/com/azmiu/Application.java
@@ -5,10 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-public class AzmiuApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(AzmiuApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
